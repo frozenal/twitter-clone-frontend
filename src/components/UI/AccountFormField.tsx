67c961456bf314bb32f2ac5e56f77164d712fa6e
frozenal <georgiajosh22@gmail.com>
2021-01-17 02:05:45 -0500

    add react hook form

--- src/components/UI/AccountFormField.tsx
+++ src/components/UI/AccountFormField.tsx
@@ -1,4 +1,10 @@
-import { Box, FormLabel, Input } from "@chakra-ui/react";
+import {
+  Box,
+  FormControl,
+  FormErrorMessage,
+  FormLabel,
+  Input,
+} from "@chakra-ui/react";
 import React, { useState } from "react";
 
 interface Props {
@@ -8,50 +14,57 @@ interface Props {
   labelId: string;
   type: string;
   ref: any;
+  errors?: string;
 }
 
 const AccountFormField = React.forwardRef((props: Props, ref: any) => {
   const [fieldSelected, setFieldSelected] = useState(false);
 
   return (
-    <Box
-      width={props.width}
-      height={16}
-      border="1px"
-      borderColor={`${!fieldSelected ? "gray.600" : "blue.400"}`}
-      rounded="md"
-      textAlign="left"
-      px={2}
-      pt={1}
-      className="border-opacity-50"
-    >
-      <FormLabel
-        textColor={!fieldSelected ? "gray.500" : "blue.400"}
-        opacity={70}
-        mb={0}
-        fontSize="sm"
+    <Box>
+      <Box
+        width={props.width}
+        height={16}
+        border="1px"
+        borderColor={`${!fieldSelected ? "gray.600" : "blue.400"}`}
+        rounded="md"
         textAlign="left"
-        width="full"
-        htmlFor={props.labelId}
+        px={2}
+        pt={1}
+        className="border-opacity-50"
       >
-        {props.label}
-      </FormLabel>
-      <Input
-        size="sm"
-        fontSize={20}
-        px={0}
-        border="none"
-        my={0}
-        bg="transparent"
-        textColor="white"
-        _focus={{ outline: "none" }}
-        name={props.name}
-        id={props.labelId}
-        type={props.type}
-        onFocus={() => setFieldSelected(true)}
-        onBlur={() => setFieldSelected(false)}
-        ref={ref}
-      />
+        <FormControl isInvalid={props.errors ? true : false}>
+          <FormLabel
+            textColor={!fieldSelected ? "gray.500" : "blue.400"}
+            opacity={70}
+            mb={0}
+            fontSize="sm"
+            textAlign="left"
+            width="full"
+            htmlFor={props.labelId}
+          >
+            {props.label}
+          </FormLabel>
+          <Input
+            size="sm"
+            fontSize={20}
+            px={0}
+            border="none"
+            my={0}
+            bg="transparent"
+            textColor="white"
+            _focus={{ outline: "none" }}
+            _invalid={{ outline: "none" }}
+            name={props.name}
+            id={props.labelId}
+            type={props.type}
+            onFocus={() => setFieldSelected(true)}
+            onBlur={() => setFieldSelected(false)}
+            ref={ref}
+          />
+          <FormErrorMessage>{props.errors}</FormErrorMessage>
+        </FormControl>
+      </Box>
     </Box>
   );
 });
--- src/components/login/LoginForm.tsx
+++ src/components/login/LoginForm.tsx
@@ -1,41 +1,65 @@
 import { Box, Flex } from "@chakra-ui/react";
 import React from "react";
+import { useForm } from "react-hook-form";
 import AccountFormField from "../UI/AccountFormField";
 import RoundBlueButton from "../UI/RoundBlueButton";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
 
 interface Props {}
 
+interface LoginFormData {
+  phoneEmailOrUsername: string;
+  password: string;
+}
+
 const LoginForm = (props: Props) => {
+  const { handleSubmit, register, errors, setError } = useForm<LoginFormData>();
+
   return (
     <Box w="35rem" h="auto">
-      <Flex flexDir="column" alignItems="center" justifyContent="center">
-        <Box py={3} px={2} w="100%">
-          <AccountFormField
-            width="100%"
-            label="Phone, email, or username"
-            name="phoneEmailOrUsername"
-            labelId="phoneEmailOrUsername"
-            type="text"
-          />
-        </Box>
-        <Box py={3} px={2} w="100%">
-          <AccountFormField
-            width="100%"
-            label="Password"
-            name="password"
-            labelId="password"
-            type="password"
-          />
-        </Box>
-        <Box py={3} px={2} w="100%">
-          <RoundBlueButton
-            submit={true}
-            text="Log in"
-            outline={false}
-            width="100%"
-          />
-        </Box>
-      </Flex>
+      <form
+        onSubmit={handleSubmit(async (formData) => {
+          console.log(formData);
+          console.log(errors);
+        })}
+      >
+        <Flex flexDir="column" alignItems="center" justifyContent="center">
+          <Box py={3} px={2} w="100%">
+            <AccountFormField
+              width="100%"
+              label="Phone, email, or username"
+              name="phoneEmailOrUsername"
+              labelId="phoneEmailOrUsername"
+              type="text"
+              ref={register({ required: true })}
+              errors={
+                errors.phoneEmailOrUsername &&
+                "Phone, email, or username required."
+              }
+            />
+          </Box>
+          <Box mt={2} py={3} px={2} w="100%">
+            <AccountFormField
+              width="100%"
+              label="Password"
+              name="password"
+              labelId="password"
+              type="password"
+              ref={register({ required: true })}
+              errors={errors.password && "Password required."}
+            />
+          </Box>
+          <Box mt={2} py={3} px={2} w="100%">
+            <RoundBlueButton
+              submit={true}
+              text="Log in"
+              outline={false}
+              width="100%"
+            />
+          </Box>
+        </Flex>
+      </form>
     </Box>
   );
 };
